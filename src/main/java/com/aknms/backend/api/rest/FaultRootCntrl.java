ad9b4004dc63732aef3fe22a83c8bcd91ce9500a
Karthik Rajagopalan <karthraj@cisco.com>
2019-02-02 10:42:55 +0530

    cross origin isse

--- src/main/java/com/aknms/backend/api/rest/FaultRootCntrl.java
+++ src/main/java/com/aknms/backend/api/rest/FaultRootCntrl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import com.aknms.backend.api.model.Event;
 import com.aknms.backend.api.model.EventTypeCount;
 
 @RestController
+@CrossOrigin
 public class FaultRootCntrl {
 
 	private static String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
@@ -29,18 +31,15 @@ public class FaultRootCntrl {
 	@RequestMapping(value = "/event/count", method = RequestMethod.GET)
 	public ResponseEntity<List<EventTypeCount>> getEventsCount() {
 		List<EventTypeCount> eventCount = faults.getEventCount();
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-		return new ResponseEntity<List<EventTypeCount>>(eventCount, headers, HttpStatus.OK);
+		
+		return new ResponseEntity<List<EventTypeCount>>(eventCount, HttpStatus.OK);
 
 	}
 
 	@RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Event> getEventById(@PathVariable int id) {
 		Event event = faults.getEventById(id);
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-		return new ResponseEntity<Event>(event, headers, HttpStatus.OK);
+		return new ResponseEntity<Event>(event, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/event", method = RequestMethod.GET)
@@ -62,9 +61,7 @@ public class FaultRootCntrl {
 		} else {
 			event = faults.getAllEvents();
 		}
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-		return new ResponseEntity<Iterable<Event>>(event, headers, HttpStatus.OK);
+		return new ResponseEntity<Iterable<Event>>(event,  HttpStatus.OK);
 
 	}
 
